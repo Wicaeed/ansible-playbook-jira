3e91c1e25eb0d092fe146015f896c2dc0f47668c
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-11-13 18:40:16 +0800

    Split Ansible installation into individual script, so AIO with customization can be more handy.

--- scripts/run-aio-build.sh
+++ scripts/run-aio-build.sh
@@ -3,16 +3,15 @@
 set -o xtrace
 
 # Install basic packages for this script.
-apt-get -y install software-properties-common
-apt-add-repository -y ppa:ansible/ansible
 apt-get update
-apt-get -y install ansible git
+apt-get -y install git
 
 # GIT clone our playbook to CWD.
 git clone https://github.com/pantarei/ansible-playbook-jira.git -b master ansible-playbook-jira
 
 # Bootstrap Ansible then run all playbooks.
 cd ansible-playbook-jira
+scripts/bootstrap-ansible.sh
 scripts/bootstrap-roles.sh
 scripts/bootstrap-vars.sh
 scripts/bootstrap-hosts.sh
