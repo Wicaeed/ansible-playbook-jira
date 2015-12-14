5ca9970018964656c030de5a03a4fc17bf9479ef
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-12-14 18:29:52 +0800

    Handle roles/* as git submodule.

--- scripts/run-aio-build.sh
+++ scripts/run-aio-build.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y install git
 
 # GIT clone our playbook to CWD.
-git clone https://github.com/pantarei/ansible-playbook-jira.git /opt/ansible-playbook-jira
+git clone --recursive https://github.com/pantarei/ansible-playbook-jira.git /opt/ansible-playbook-jira
 cd /opt/ansible-playbook-jira
 
 # Bootstrap Ansible then run all playbooks.
